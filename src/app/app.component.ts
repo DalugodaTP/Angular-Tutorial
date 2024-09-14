ede4274daaeccc6f14ef15bd018af03eb820abd5
Tharindu <Dalugodatp@gmail.com>
2024-09-14 19:14:12 +0530

    Dynamically access components using template reference

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild,ViewContainerRef,AfterViewInit } from '@angular/core';
+import { RoomsComponent } from './rooms/rooms.component';
 
 @Component({
   selector: 'hinv-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit{
+
+
   title = 'hotelinventoryapp';
 
-  role='Admin';
+  // role='Admin';
   // role='None';
   // role='User';
+  @ViewChild('user',{read: ViewContainerRef}) vcr!: ViewContainerRef;
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    const componentRef = this.vcr.createComponent(RoomsComponent);
+    componentRef.instance.numberOfRooms =50;
+  }
+
 }
